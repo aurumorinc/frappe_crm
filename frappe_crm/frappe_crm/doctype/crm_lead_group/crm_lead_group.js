0379a3bf86dae9550b0c995865fc471e097ed046
Aryan Singh <aquiveal@gmail.com>
2026-07-21 08:06:08 +0530

    feat: add CRM Lead Group doctype
    Create a new CRM Lead Group doctype to manage lead grouping functionality. This doctype includes a group name field (required and unique) and an optional description field. The implementation provides basic CRUD operations with System Manager permissions and supports bulk editing and renaming.

---
+++ frappe_crm/frappe_crm/doctype/crm_lead_group/crm_lead_group.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Aryan Singh and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("CRM Lead Group", {
+// 	refresh(frm) {
+
+// 	},
+// });
